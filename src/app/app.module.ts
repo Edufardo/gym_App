20d0dcdedcda3c9d6d29b0ae78eae2b540b0b9b2
edufardo <eduardo.larraz.labordeta@gmail.com>
2021-11-16 22:20:21 +0100

    ADD - New training form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { TrainingComponent } from './components/training/training.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    TrainingComponent
   ],
   imports: [
     BrowserModule,
